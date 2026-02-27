4143fad05a2be3e286cacc68fbb79a8bc4689613
jakubwisniewski <jakub.wisniewski@supermetrics.com>
2026-02-27 22:13:29 +0100

    Add H2 database console, update configs, and adjust security settings
    - Added `spring-boot-h2console` dependency in `pom.xml`.
    - Enabled H2 console with custom path `/h2-console` and adjusted JDBC settings in `application.yaml`.
    - Updated `SecurityConfig` to permit H2 console access, disable frame options, and exclude H2 console requests from CSRF protection.

--- src/main/java/eu/wisniewska/www/config/SecurityConfig.java
+++ src/main/java/eu/wisniewska/www/config/SecurityConfig.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -26,6 +25,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(
+                        csrf -> csrf.ignoringRequestMatchers("/h2-console/**")
+                )
+                .headers(
+                        headers -> headers
+                                .frameOptions(
+                                        HeadersConfigurer.FrameOptionsConfig::disable
+                                )
+                )
                 .formLogin(
                         formLogin -> formLogin
                                 .defaultSuccessUrl("/_admin")
@@ -37,6 +45,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         authorizeRequests -> authorizeRequests
                                 .requestMatchers("/actuator/**").permitAll()
+                                .requestMatchers("/h2-console/**").permitAll()
                                 .requestMatchers("/_admin/**").hasRole(ROLE_ADMIN)
                                 .anyRequest().permitAll()
                 )
