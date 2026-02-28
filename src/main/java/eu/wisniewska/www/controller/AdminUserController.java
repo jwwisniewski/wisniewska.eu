e99ae81ebf5db40e4210d3f818869d9278a3c4d6
jakubwisniewski <jakub.wisniewski@supermetrics.com>
2026-02-28 21:37:53 +0100

    Simplify `AdminUserController` routes and extract common model attributes into a dedicated `@ModelAttribute` method

--- src/main/java/eu/wisniewska/www/controller/AdminUserController.java
+++ src/main/java/eu/wisniewska/www/controller/AdminUserController.java
@@ -3,21 +3,27 @@ package eu.wisniewska.www.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/_admin/users")
 public class AdminUserController {
 
-    @GetMapping("/_admin/users")
-    public String index(Model model) {
+    @ModelAttribute()
+    public void commonAttributes(Model model) {
         model.addAttribute("activePage", "users");
+    }
+
+    @GetMapping("")
+    public String index(Model model) {
         model.addAttribute("pageTitle", "Admin User Listing");
 
         return "admin/users/index";
     }
 
-    @GetMapping("/_admin/users/add")
+    @GetMapping("/add")
     public String add(Model model) {
-        model.addAttribute("activePage", "users");
         model.addAttribute("pageTitle", "Adding a new Admin User");
 
         return "admin/users/add";
